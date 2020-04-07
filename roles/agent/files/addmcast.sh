6d2c2c29b728b72a74f911a06c2285400f97fa54
Tu Pham <tupm4@viettel.com.vn>
2020-04-07 15:40:11 +0700

    Refactor project structure

--- roles/agent/files/addmcast.sh
+++ roles/agent/files/addmcast.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for intf in `ls -l /sys/class/net/ | grep "\->" | grep -v virtual | awk '{print $9}'`
 do
   /sbin/ip l set $intf up
@@ -6,6 +7,6 @@ do
   if [ ! -n "$DOWN" ] ; then
     /sbin/ip maddress add 01:80:c2:00:00:0e dev $intf
     /sbin/ip maddress add 01:80:c2:00:00:03 dev $intf
-    /sbin/ip maddress add 01:80:c2:00:00:00 dev $intf   
+    /sbin/ip maddress add 01:80:c2:00:00:00 dev $intf
   fi
 done
